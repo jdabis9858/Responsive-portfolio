bed29f0b2f2c48f61ae2b5de56eb065e6ce48683
jdabis9858 <jdabis4281994@gmail.com>
2018-11-19 18:00:28 -0800

    added jaavascript functionality

---
+++ portfolio.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+    // $(".btn").on("click", function() {
+    //     window.location.replace("file:///C:/Users/jdabi/OneDrive/Desktop/Responsive-portfolio/portfolio.html")
+    // })
+
+    $("#abt").on("click", function() {
+        window.location.replace("file:///C:/Users/jdabi/OneDrive/Desktop/Responsive-portfolio/index-p.html")
+    })
+
+    $("#prt").on("click", function() {
+        window.location.replace("file:///C:/Users/jdabi/OneDrive/Desktop/Responsive-portfolio/portfolio.html")
+    })
+
+    $("#cntct").on("click", function() {
+        window.location.replace("file:///C:/Users/jdabi/OneDrive/Desktop/Responsive-portfolio/contact.html")
+    })
+
+});
