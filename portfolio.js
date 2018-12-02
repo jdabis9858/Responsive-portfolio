0f445557da8d16f2ae04d7b82b3a069a359a33bc
jdabis9858 <jdabis4281994@gmail.com>
2018-12-02 05:11:00 -0800

    added to portfolio 1

--- portfolio.js
+++ portfolio.js
@@ -3,6 +3,10 @@ $(document).ready(function () {
     // $(".btn").on("click", function() {
     //     window.location.replace("file:///C:/Users/jdabi/OneDrive/Desktop/Responsive-portfolio/portfolio.html")
     // })
+    $(".imgtxt2").on("click", function() {
+        window.location.replace("https://github.com/jdabis9858/liri-node-app")
+        console.log("clicked")
+    })
 
     $("#abt").on("click", function() {
         window.location.replace("file:///C:/Users/jdabi/OneDrive/Desktop/Responsive-portfolio/index-p.html")
